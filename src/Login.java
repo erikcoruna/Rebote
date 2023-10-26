b1edc36bbfd53291076d8062d9fd8bca09545d82
Erik Coruña <erik.coruna@opendeusto.es>
2023-10-26 20:44:10 +0200

    Depuración de código.

--- src/Login.java
+++ src/Login.java
@@ -1,8 +1,12 @@
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-//import java.awt.GridLayout;
-//import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,84 +19,77 @@ import javax.swing.JTextField;
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
-	public static void main(String[] args) {
-		new Login();
-	}
 
     public Login() {
-		setSize(480, 480);
+		setSize(480, 560);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Log In");
+		setTitle("Iniciar Sesión");
 	
+		// https://stackoverflow.com/questions/42964669/placing-button-panel-in-center-java-swing
+        // Se ha modificado parte del código para adecuarlo a nuestro proyecto con los botones y textos necesarios.
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		gbc.anchor = GridBagConstraints.NORTH;
 		gbc.anchor = GridBagConstraints.CENTER;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-	
-		JPanel registro = new JPanel(new GridBagLayout());
 		
-		JLabel lname = new JLabel("Introduzca un nombre de usuario:");
-		JTextField tname = new JTextField();
-		tname.setPreferredSize(new Dimension(300, 20));
-		JLabel lpass = new JLabel("Introduzca su contraseña:");
-		JPasswordField ppass = new JPasswordField();
-		ppass.setPreferredSize(new Dimension(300, 20));
-		JLabel forget = new JLabel("¿Ha olvidado su contraseña?");
+		JLabel labelName = new JLabel("Introduzca un nombre de usuario:");
+		JTextField textFieldName = new JTextField();
+		textFieldName.setPreferredSize(new Dimension(300, 20));
+		JLabel labelPassword = new JLabel("Introduzca su contraseña:");
+		JPasswordField passwordFieldPassword = new JPasswordField();
+		passwordFieldPassword.setPreferredSize(new Dimension(300, 20));
+		JLabel labelForget = new JLabel("¿Ha olvidado su contraseña?");
+		// https://stackoverflow.com/questions/2715118/how-to-change-the-size-of-the-font-of-a-jlabel-to-take-the-maximum-size
+		// Para cambiar el tamaño de la fuente.
+		labelForget.setFont(new Font(labelForget.getFont().getName(), Font.PLAIN, 10));
 		
-		registro.add(lname, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(tname, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(lpass, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(ppass, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(forget, gbc);
-		registro.add(new JLabel(" "), gbc);
+		panel.add(labelName, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(textFieldName, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(labelPassword, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(passwordFieldPassword, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(labelForget, gbc);
 		
-		for (int i = 0; i < 4; i++) {
-			registro.add(new JLabel(" "), gbc);
+		for (int i = 0; i < 3; i++) {
+			panel.add(new JLabel(" "), gbc);
 		}
 		
-	
-		GridBagConstraints gbc2 = new GridBagConstraints();
-		gbc2.anchor = GridBagConstraints.SOUTH;
-		gbc2.anchor = GridBagConstraints.EAST;
-	
-		JPanel acept = new JPanel(new GridBagLayout());
-		JButton confirmar = new JButton("Confirmar");
-		JButton cancelar = new JButton("Cancelar");
-	
-		acept.add(confirmar, gbc2);
-		acept.add(cancelar, gbc2);
-		panel.add(registro, gbc);
-		panel.add(acept, gbc2);
-		add(panel);
+		JButton confirm = new JButton("Confirmar");
+		JButton cancel = new JButton("Cancelar");
+
+		panel.add(confirm, gbc);
+		panel.add(cancel, gbc);
 		
+		add(panel);
 		setVisible(true);
+		
+		// https://github.com/andoni-eguiluz/ud-progII-2023/blob/master/Clase2023-2/src/tema5/resueltos/ej5b8/VentanaPrincipal.java
+        // Cogido la parte de los ActionListener y modificado para nuestro código.
+//		confirm.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				
+//			}
+//		});
+		cancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Start windowStart = new Start();
+				windowStart.setVisible(true);
+				setVisible(false);
+			}
+		});
+//		labelForget.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				
+//			}
+//		});
     }
-    
-//  botonIniciarSesion.addActionListener(new ActionListener() {
-//  @Override
-//  public void actionPerformed(ActionEvent e) {
-//      String usuario = usuarioField.getText();
-//      char[] contrasena = contrasenaField.getPassword();
-//      // Aquí puedes agregar la lógica de autenticación
-//      
-//      if (usuario.equals("usuarioEjemplo") && String.valueOf(contrasena).equals("contrasenaEjemplo")) {
-//          JOptionPane.showMessageDialog(frame, "Inicio de sesión exitoso");
-//      } else {
-//          JOptionPane.showMessageDialog(frame, "Inicio de sesión fallido");
-//      }
-//      
-//      // Limpia los campos después del intento de inicio de sesión
-//      usuarioField.setText("");
-//      contrasenaField.setText("");
-//  }
-//});
 }
--- src/Register.java
+++ src/Register.java
@@ -21,81 +21,80 @@ public class Register extends JFrame {
 		setSize(480, 560);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Log In");
+		setTitle("Registrarse");
 
+		// https://stackoverflow.com/questions/42964669/placing-button-panel-in-center-java-swing
+        // Se ha modificado parte del código para adecuarlo a nuestro proyecto con los botones y textos necesarios.
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
-
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		gbc.anchor = GridBagConstraints.NORTH;
 		gbc.anchor = GridBagConstraints.CENTER;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 
-		JPanel registro = new JPanel(new GridBagLayout());
-
-		JLabel nombreUsuario = new JLabel("Introduzca un nombre de usuario:");
-		JTextField nombre = new JTextField();
-		nombre.setPreferredSize(new Dimension(300, 20));
-		JLabel nombreContrasenia = new JLabel("Introduzca su contraseña:");
-		JPasswordField contrasenia = new JPasswordField();
-		contrasenia.setPreferredSize(new Dimension(300, 20));
-		JLabel nombreNContrasenia = new JLabel("Introduzca de nuevo su contraseña:");
-		JPasswordField Ncontrasenia = new JPasswordField();
-		Ncontrasenia.setPreferredSize(new Dimension(300, 20));
-		JLabel eleccion = new JLabel("Elija su rol:");
-		JRadioButton entrenador = new JRadioButton("Entrenador");
-		JRadioButton jugador = new JRadioButton("Jugador");
+		JLabel labelName = new JLabel("Introduzca un nombre de usuario:");
+		JTextField textFieldName = new JTextField();
+		textFieldName.setPreferredSize(new Dimension(300, 20));
+		JLabel labelPassword = new JLabel("Introduzca su contraseña:");
+		JPasswordField passwordFieldPassword = new JPasswordField();
+		passwordFieldPassword.setPreferredSize(new Dimension(300, 20));
+		JLabel labelPassword2 = new JLabel("Introduzca de nuevo su contraseña:");
+		JPasswordField passwordFieldPassword2 = new JPasswordField();
+		passwordFieldPassword2.setPreferredSize(new Dimension(300, 20));
+		JLabel labelRole = new JLabel("Elija su rol:");
+		JRadioButton coach = new JRadioButton("Entrenador");
+		JRadioButton player = new JRadioButton("Jugador");
 
 		ButtonGroup grupo = new ButtonGroup();
-		grupo.add(entrenador);
-		grupo.add(jugador);
+		grupo.add(coach);
+		grupo.add(player);
 
-		registro.add(nombreUsuario, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(nombre, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(nombreContrasenia, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(contrasenia, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(nombreNContrasenia, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(Ncontrasenia, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(eleccion, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(entrenador, gbc);
-		registro.add(jugador, gbc);
-		registro.add(new JLabel(" "), gbc);
-		registro.add(new JLabel(" "), gbc);
+		panel.add(labelName, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(textFieldName, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(labelPassword, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(passwordFieldPassword, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(labelPassword2, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(passwordFieldPassword2, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(labelRole, gbc);
+		panel.add(new JLabel(" "), gbc);
+		panel.add(coach, gbc);
+		panel.add(player, gbc);
+		
+		for (int i = 0; i < 3; i++) {
+			panel.add(new JLabel(" "), gbc);
+		}
 
-		GridBagConstraints gbc2 = new GridBagConstraints();
-		gbc2.anchor = GridBagConstraints.SOUTH;
-		gbc2.anchor = GridBagConstraints.EAST;
-
-		JPanel acept = new JPanel(new GridBagLayout());
-		JButton OK = new JButton("Confirmar");
-		JButton back = new JButton("Cancelar");
-		OK.addActionListener(new ActionListener() {
-			
+		JButton confirm = new JButton("Confirmar");
+		JButton cancel = new JButton("Cancelar");
+		
+		panel.add(confirm, gbc);
+		panel.add(cancel, gbc);
+		
+		add(panel);
+		setVisible(true);
+		
+//		confirm.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// TODO Auto-generated method stub
+//				//CHEQUEAR QUE SE HAYA RELLENADO TODO CORRECTAMENTE
+//				//CREAR un user y registrarlo en el fichero de logger con el método toString User.logger()
+//				//fastRegister();
+//			}
+//		});
+		cancel.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				//CHEQUEAR QUE SE HAYA RELLENADO TODO CORRECTAMENTE
-				//CREAR un user y registrarlo en el fichero de logger con el método toString User.logger()
-				//fastRegister();
+				Start windowStart = new Start();
+				windowStart.setVisible(true);
+				setVisible(false);
 			}
 		});
-		
-		
-		
-		acept.add(OK, gbc2);
-		acept.add(back, gbc2);
-		panel.add(registro, gbc);
-		panel.add(acept, gbc2);
-		add(panel);
-
-		setVisible(true);
 	}
 }
