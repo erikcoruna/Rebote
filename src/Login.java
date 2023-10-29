98a4ccff4099cf999876c538e514b4c54c0cca03
HugoRey <hugo.rey@opendeusto.es>
2023-10-29 22:41:57 +0100

    Eliminacion de Warnings

--- src/Login.java
+++ src/Login.java
@@ -1,13 +1,9 @@
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
