fddebe9628816b2aed8ab0820ec4de48c4650728
Erik Coruña <erik.coruna@opendeusto.es>
2023-11-04 13:53:14 +0100

    Añadido JProgressBar al inicio de la aplicación

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
-public class Main {
+public class Main extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	// https://www.geeksforgeeks.org/java-swing-jprogressbar/
+	// Se han hecho algunas modificaciones para adecuarlo a nuestra aplicación
+	static JFrame frame = new JFrame("Cargando...");
+	static JPanel panel = new JPanel();
+	static JProgressBar progressBar = new JProgressBar();
+	
     public static void main(String[] args) {
+    	frame.setSize(300, 70);
+    	frame.setLocationRelativeTo(null);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+    	progressBar.setValue(0);
+    	progressBar.setStringPainted(true);
+    	panel.add(progressBar);
+    	frame.add(panel);
+    	
+    	frame.setVisible(true);
+    	
+    	fill();
+
+    	frame.dispose();
+    	
         // Ventana inicial
     	new Start();
+    }
+    
+    public static void fill() {
+    	int i = 0;
+    	while (i <= 100) {
+    		progressBar.setValue(i + 1);
+    		try {
+				Thread.sleep(30);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    		i += 1;
+    	}
     }
 }
