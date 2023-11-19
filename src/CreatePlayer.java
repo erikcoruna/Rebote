88355d7659b8bcc9b1d71d4e6b5bf2f1975671b2
HugoRey <hugo.rey@opendeusto.es>
2023-11-19 18:54:45 +0100

    Correcion de errores

--- src/CreatePlayer.java
+++ src/CreatePlayer.java
@@ -241,14 +241,16 @@ public class CreatePlayer extends JFrame {
 		
 		try (FileWriter writer = new FileWriter("files\\players.csv", true)){
 			writer.write(username + "," + surname1 + "," + surname2 + "," + dorsalNumber + "," + height + "," + date);
-			nameTextField.setText("");
-			surname1TextField.setText("");
-			surname2TextField.setText("");
-			dorsalNumberTextField.setText("");
-			heightSpinner.setValue(150);
+			
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(this, "No se ha podido guardar en el fichero", "ERROR", JOptionPane.ERROR_MESSAGE);
 			e.printStackTrace();
 		}
+		
+		nameTextField.setText("");
+		surname1TextField.setText("");
+		surname2TextField.setText("");
+		dorsalNumberTextField.setText("");
+		heightSpinner.setValue(150);
 	}
 }
