3f3c42c5c689b118e76deceb2bfe2069155eb9ac
Erik Coruña <erik.coruna@opendeusto.es>
2023-11-21 20:48:28 +0100

    Creado archivo de configuración con clase para leer la configuración.

---
+++ src/ConfigReader.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    public static void main(String[] args) {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("config.properties");
+
+            prop.load(input);
+
+            String projectVersion = prop.getProperty("version");
+            String projectName = prop.getProperty("name");
+
+            System.out.println("Versión del proyecto: " + projectVersion);
+            System.out.println("Nombre del proyecto: " + projectName);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
