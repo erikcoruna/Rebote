3522242c5fd89bb9d823e5f4367a4be796caf1bc
Erik Coruña <erik.coruna@opendeusto.es>
2023-11-26 23:18:36 +0100

    Separación de renderer y añadido renderer para el header de la tabla.

--- src/GameTableHeaderRenderer.java
+++ src/GameTableHeaderRenderer.java
@@ -1,22 +1,30 @@
 import java.awt.Component;
-import java.awt.Point;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
-public class GameTableRenderer implements TableCellRenderer {
+public class GameTableHeaderRenderer implements TableCellRenderer {
 
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
 			int row, int column) {
 		JLabel label = new JLabel();
-		if (column == 4) {
-			Point point = (Point) value;
-			label.setText("x=" + point.x + " y=" + point.y);
-		} else {
-			label.setText(value.toString());
-		}
+
+		// Erik Coruña Rodríguez_2023-11-26_23-15.txt
+		// Se han modificado algunas cosas en el código.
+        ImageIcon icon = new ImageIcon("img/" + value + ".png");
+        Image scaledImage = icon.getImage().getScaledInstance(90, 30, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        label.setIcon(scaledIcon);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        
+        label.setOpaque(true);
+		
 		return label;
 	}
 
