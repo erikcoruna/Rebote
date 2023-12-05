b4aff24b9ff974e21754ac08ac86cbab6bab2e98
Erik Coruña <erik.coruna@opendeusto.es>
2023-12-05 22:46:41 +0100

    Depuración del código.

--- src/io/CSVFileManager.java
+++ src/io/CSVFileManager.java
@@ -11,8 +11,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
@@ -34,15 +32,22 @@ public class CSVFileManager implements IFileManager {
 	
 	public String[] playerHeaders = getAttributeNames(Player.class);
 	public String[] trainerHeaders = getAttributeNames(Trainer.class);
+	private SimpleDateFormat isoDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 	public static void main(String[] args) {
 		CSVFileManager fileManager = new CSVFileManager();
 		List<Trainer> trainersList = new ArrayList<>();
 		Vector<String> teams = new Vector<>(Arrays.asList("Test", "Test2"));
-		trainersList.add(new Trainer(1, "Erik", "Coruña", "Rodríguez", "prueba", new GregorianCalendar(2004, 22, 4), "España", teams));
-		trainersList.add(new Trainer(2, "Ander", "Herrero", "Pascual", "prueba", new GregorianCalendar(2004, 24, 7), "Francia", teams));
+		trainersList.add(new Trainer(1, "erik.coruna", "Erik", "Coruña", "Rodríguez", "prueba", new GregorianCalendar(2004, 22, 4), "España", teams));
+		trainersList.add(new Trainer(2, "ander.herrero", "Ander", "Herrero", "Pascual", "prueba", new GregorianCalendar(2004, 24, 7), "Francia", teams));
+		
+		List<Player> playersList = new ArrayList<>();
+		playersList.add(new Player(1, "erik.coruna", "Erik", "Coruña", "Rodríguez", "prueba", new GregorianCalendar(2004, 22, 4), "España", "A1", 170, 60.3f, "Test"));
+		playersList.add(new Player(2, "ander.herrero", "Ander", "Herrero", "Pascual", "prueba", new GregorianCalendar(2004, 24, 7), "Francia", "A2", 196, 75.4f, "Test2"));
 		try {
 			fileManager.exportTrainersToFile(trainersList, Paths.get("src/io/trainers.csv"));
-			fileManager.importTrainersFromFile(Paths.get("src/io/trainers.csv"));
+			System.out.println(fileManager.importTrainersFromFile(Paths.get("src/io/trainers.csv")));
+			fileManager.exportPlayersToFile(playersList, Paths.get("src/io/players.csv"));
+			System.out.println(fileManager.importPlayersFromFile(Paths.get("src/io/players.csv")));
 		} catch (UserRepositoryException e) {
 			e.printStackTrace();
 		}
@@ -65,33 +70,40 @@ public class CSVFileManager implements IFileManager {
         return attributeNames.toArray(new String[0]);
     }
 	
+	private GregorianCalendar stringToCalendar(String str) {
+		GregorianCalendar calendar = new GregorianCalendar();
+		try {
+			calendar.setTime(isoDateFormat.parse(str));
+		} catch (ParseException e) {
+			calendar.setTime(null);
+		}
+		return calendar;
+	}
+	
+	private String calendarToString(GregorianCalendar calendar) {
+		return isoDateFormat.format(calendar.getTime());
+	}
+	
 	@Override
 	public List<Player> importPlayersFromFile(Path path) throws UserRepositoryException {
 		try (ICsvMapReader csvReader = new CsvMapReader(new FileReader("src/io/players.csv"), CsvPreference.STANDARD_PREFERENCE)) {
 			Map<String, String> row;
 			List<Player> playerList = new ArrayList<>();
-			SimpleDateFormat isoDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			
 			csvReader.getHeader(false);
 			while((row = csvReader.read(playerHeaders)) != null) {
 				Player player = new Player();
 				player.setId(Integer.parseInt(row.get("id")));
+				player.setUsername(row.get("username"));
 				player.setName(row.get("name"));
 				player.setFirstSurname(row.get("firstSurname"));
 				player.setSecondSurname(row.get("secondSurname"));
 				player.setPassword(row.get("password"));
-				try {
-					Date date = isoDateFormat.parse(row.get("birthDate"));
-					GregorianCalendar calendar = new GregorianCalendar();
-					calendar.setTime(date);
-					player.setBirthDate(calendar);
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
+				player.setBirthDate(stringToCalendar(row.get("birthDate")));
 				player.setCountry(row.get("country"));
 				player.setCategory(row.get("category"));
 				player.setHeight(Integer.parseInt(row.get("height")));
-				player.setWeight(Integer.parseInt(row.get("weight")));
+				player.setWeight(Float.parseFloat(row.get("weight")));
 				player.setTeam(row.get("team"));
 				playerList.add(player);
 			}
@@ -107,28 +119,23 @@ public class CSVFileManager implements IFileManager {
 		try (ICsvMapReader csvReader = new CsvMapReader(new FileReader("src/io/trainers.csv"), CsvPreference.STANDARD_PREFERENCE)) {
 			Map<String, String> row;
 			List<Trainer> trainersList = new ArrayList<>();
-			SimpleDateFormat isoDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			
 			csvReader.getHeader(false);
 			while((row = csvReader.read(trainerHeaders)) != null) {
 				Trainer trainer = new Trainer();
 				trainer.setId(Integer.parseInt(row.get("id")));
+				trainer.setUsername(row.get("username"));
 				trainer.setName(row.get("name"));
 				trainer.setFirstSurname(row.get("firstSurname"));
 				trainer.setSecondSurname(row.get("secondSurname"));
 				trainer.setPassword(row.get("password"));
-				System.out.println(row.get("birthDate"));
-				try {
-					Date date = isoDateFormat.parse(row.get("birthDate"));
-					GregorianCalendar calendar = new GregorianCalendar();
-					calendar.setTime(date);
-					trainer.setBirthDate(calendar);
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
+				trainer.setBirthDate(stringToCalendar(row.get("birthDate")));
 				trainer.setCountry(row.get("country"));
-				String trainerTeams = row.get("teams");
-				System.out.println(trainerTeams);
+				Vector<String> teams = new Vector<String>();
+				for (String team : row.get("teams").split(",")) {
+					teams.add(team.replaceAll("[\\[\\]]", ""));
+				}
+				trainer.setTeams(teams);
 				trainersList.add(trainer);
 			}
 			return trainersList;
@@ -147,16 +154,17 @@ public class CSVFileManager implements IFileManager {
 			for (Player player : players) {
 				Map<String, String> row = new HashMap<>();
 				row.put(playerHeaders[0], String.valueOf(player.getId()));
-				row.put(playerHeaders[1], player.getName());
-				row.put(playerHeaders[2], player.getFirstSurname());
-				row.put(playerHeaders[3], player.getSecondSurname());
-				row.put(playerHeaders[4], player.getPassword());
-				row.put(playerHeaders[5], String.valueOf(player.getBirthDate()));
-				row.put(playerHeaders[6], player.getCountry());
-				row.put(playerHeaders[7], player.getCategory());
-				row.put(playerHeaders[8], String.valueOf(player.getHeight()));
-				row.put(playerHeaders[9], String.valueOf(player.getWeight()));
-				row.put(playerHeaders[10], player.getTeam());
+				row.put(playerHeaders[1], player.getUsername());
+				row.put(playerHeaders[2], player.getName());
+				row.put(playerHeaders[3], player.getFirstSurname());
+				row.put(playerHeaders[4], player.getSecondSurname());
+				row.put(playerHeaders[5], player.getPassword());
+				row.put(playerHeaders[6], calendarToString(player.getBirthDate()));
+				row.put(playerHeaders[7], player.getCountry());
+				row.put(playerHeaders[8], player.getCategory());
+				row.put(playerHeaders[9], String.valueOf(player.getHeight()));
+				row.put(playerHeaders[10], String.valueOf(player.getWeight()));
+				row.put(playerHeaders[11], player.getTeam());
 				
 				csvWriter.write(row, playerHeaders);
 			}
@@ -173,13 +181,14 @@ public class CSVFileManager implements IFileManager {
 			for (Trainer trainer : trainers) {
 				Map<String, String> row = new HashMap<>();
 				row.put(trainerHeaders[0], String.valueOf(trainer.getId()));
-				row.put(trainerHeaders[1], trainer.getName());
-				row.put(trainerHeaders[2], trainer.getFirstSurname());
-				row.put(trainerHeaders[3], trainer.getSecondSurname());
-				row.put(trainerHeaders[4], trainer.getPassword());
-				row.put(trainerHeaders[5], String.valueOf(trainer.getBirthDate()));
-				row.put(trainerHeaders[6], trainer.getCountry());
-				row.put(trainerHeaders[7], trainer.getTeams().toString());
+				row.put(trainerHeaders[1], trainer.getUsername());
+				row.put(trainerHeaders[2], trainer.getName());
+				row.put(trainerHeaders[3], trainer.getFirstSurname());
+				row.put(trainerHeaders[4], trainer.getSecondSurname());
+				row.put(trainerHeaders[5], trainer.getPassword());
+				row.put(trainerHeaders[6], calendarToString(trainer.getBirthDate()));
+				row.put(trainerHeaders[7], trainer.getCountry());
+				row.put(trainerHeaders[8], trainer.getTeams().toString());
 				
 				csvWriter.write(row, trainerHeaders);
 			}
