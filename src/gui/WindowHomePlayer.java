b2075b757161a248ffbc48ee1e2d05a9e07d440d
Hugo Rey <hugo.rey@opendeusto.es>
2024-01-03 17:48:11 +0100

    Correccion de Warnings

--- src/gui/WindowHomePlayer.java
+++ src/gui/WindowHomePlayer.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -30,7 +29,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 
 import com.toedter.calendar.JDateChooser;
 
