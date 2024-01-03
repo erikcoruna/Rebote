dd4acbd6c1c4f888524d71754ad56dce4f2a9ed6
Erik Coruña <erik.coruna@opendeusto.es>
2024-01-04 00:26:04 +0100

    Estructura de la ventana WindowTeam.

--- src/gui/WindowTeam.java
+++ src/gui/WindowTeam.java
@@ -1,8 +1,13 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import domain.League;
@@ -19,10 +24,34 @@ public class WindowTeam extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle(team.getName());
+		setIconImage(new ImageIcon("src/img/" + team.getLeague() + ".png").getImage());
 		
 		JPanel panel = new JPanel(new BorderLayout());
 		
+		JLabel nameLabel = new JLabel(team.getName());
+		// https://chuidiang.org/index.php?title=Fuentes_de_texto_en_Java
+		// Para coger la fuente.
+		nameLabel.setFont(new Font("Agency FB", Font.BOLD, 30));
+		nameLabel.setHorizontalAlignment(JLabel.CENTER);
+		panel.add(nameLabel, BorderLayout.NORTH);
 		
+		// https://stackoverflow.com/questions/9660987/how-to-get-a-tab-character
+		// Para coger el caracter tab en html.
+		JLabel stuffLabel = new JLabel(String.format("<html>Ciudad: %s&#9;&#9;Estadio: %s<br/><br/>Descripción: %s",
+				team.getCity(),
+				team.getStadium(),
+				team.getDescription()));
+		stuffLabel.setVerticalAlignment(JLabel.NORTH);
+		stuffLabel.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 0));
+		panel.add(stuffLabel, BorderLayout.CENTER);
+		
+		JPanel southPanel = new JPanel(new BorderLayout());
+		southPanel.setBackground(Color.PINK);
+		JLabel gamesPlayedLabel = new JLabel("Partidos jugados");
+		gamesPlayedLabel.setHorizontalAlignment(JLabel.CENTER);
+		gamesPlayedLabel.setFont(new Font("Agency FB", Font.BOLD, 20));
+		southPanel.add(gamesPlayedLabel, BorderLayout.NORTH);
+		panel.add(southPanel, BorderLayout.SOUTH);
 		
 		add(panel);
 		
