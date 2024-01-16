e719639fb7f328f36a6dc9f118e1cd5427f9889c
ander <ander@PCHerrero>
2024-01-16 16:02:22 +0100

    Añadido keyListener para eliminar el último elemento añadido al realizar ctrl+"z"

--- src/gui/WindowGameRegister.java
+++ src/gui/WindowGameRegister.java
@@ -5,6 +5,9 @@ import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.Connection;
@@ -124,6 +127,16 @@ public class WindowGameRegister extends JFrame {
 		tableActions.getTableHeader().setDefaultRenderer(new GameTableHeaderRenderer());
 		JScrollPane scrollPane = new JScrollPane(tableActions);
 		tableActions.setRowHeight(30);
+		tableActions.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				 if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_Z) {
+					 incidents.remove(0);
+					 gameTableModel.fireTableDataChanged();
+				 }
+			}
+		});
 		panelTable.add(scrollPane);
 		
 		// Botonera con opciones en la parte de abajo a la derecha
